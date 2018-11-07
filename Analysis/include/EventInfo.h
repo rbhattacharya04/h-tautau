c6b444db39350a0ecfb687a1b88a0d916609f476
rbhattacharya04 <rajarshi.bhattacharya@cern.ch>
2018-11-07 13:20:22 +0530

    Disabling kinfit

--- Analysis/include/EventInfo.h
+++ Analysis/include/EventInfo.h
@@ -483,6 +483,7 @@ public:
 
     const kin_fit::FitResults& GetKinFitResults()
     {
+        return 1;
         Lock lock(*mutex);
         if(!HasBjetPair())
             throw exception("Can't retrieve KinFit results.");
